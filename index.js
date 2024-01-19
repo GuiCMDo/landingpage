475750041d5ae6a7a0f15e0543626452d8d9edcb
Guilherme C M Dourado <guilhermecmdourado@outlook.com.br>
2024-01-19 02:19:29 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function functionScroll() {
+    window.scroll({
+        top: 600,
+        // left: 100,
+        behavior: "smooth",
+      });
+}
+
+function github() {
+    Confirm("Encaminhando para o github!");
+    
+}
